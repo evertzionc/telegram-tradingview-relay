505d85bd6f6b1dfb7b45ef272ba9dfd31c59935f
evertzionc <evertzionc@gmail.com>
2025-08-04 00:51:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,20 +9,10 @@ app = Flask(__name__)
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
-print(f"Loaded TELEGRAM_TOKEN: {TELEGRAM_TOKEN}")
-print(f"Loaded TELEGRAM_CHAT_ID: {TELEGRAM_CHAT_ID}")
-
-# Test route to check logging works
-@app.route('/test', methods=['GET'])
-def test():
-    print("Test endpoint hit!", file=sys.stdout, flush=True)
-    return 'Test OK', 200
-
 # Webhook endpoint that TradingView will post to
 @app.route('/webhook', methods=['POST'])
 def webhook():
     data = request.json
-    print("Received webhook data:", data, , file=sys.stdout, flush=True)
     message = data.get("text", str(data))  # If 'text' isn't in JSON, use raw data
     try:
         send_to_telegram(message)
@@ -39,10 +29,7 @@ def send_to_telegram(text):
         'text': text,
         'parse_mode': 'Markdown'
     }
-    print("Sending message to Telegram:", text, file=sys.stdout, flush=True)
     response = requests.post(url, json=payload)
-    print("Telegram API Status:", response.status_code , file=sys.stdout, flush=True)
-    print("Telegram API Response:", response.text,file=sys.stdout, flush=True)
     response.raise_for_status()  # Raises an error if status code is not 200
 
 # Entry point for running locally
